bae190b71d9526283aaa2ccb82bdf484d7f4c322
pangduckwai <paul.lai@sea9.org>
2021-06-28 17:40:25 +0800

    Change to a more readable version

--- tictactoe.js
+++ tictactoe.js
@@ -1,169 +1,150 @@
 
-function evaluate(game) {
-	const lgth = game.cells.length;
-	let hv; // horizontal or vertical
-	let df = 0, db = 0; // diagonal
-	let dw = 0; // draw
+// Game Status
+// player: indicate which player's turn it is
+//  - player == -1 => 'X'
+//  - player == +1 => 'O'
+// cells: n x n grid with value: 0 (unoccupied), -1 (occupied by 'X'), +1 (occupied by 'O')
+// moves: total count
+// wins: statistic for all winning conditions (n + n + 2, that is each horizontal and vertical lines plus the 2 diagonals)
+//  - 0: forward diagonal, 1: backward diagonal, the rest are horizontals then vertical
+const game = {
+	player: 0,
+	cells: [[0]],
+	moves: 0,
+	wins: [0],
+}
 
-	// horizontal and diagonal
-	for (let i = 0; i < lgth; i ++) {
-		hv = 0;
-		for (let j = 0; j < lgth; j ++) {
-			const isFrwDia = (i === j);
-			const isBckDia = ((i + j + 1) === lgth);
-			if (game.cells[i][j] > 0) {
-				hv ++;
-				if (isFrwDia) df ++;
-				if (isBckDia) db ++;
-			} else if (game.cells[i][j] < 0) {
-				hv --;
-				if (isFrwDia) df --;
-				if (isBckDia) db --;
+function evaluate(player, row, col) {
+	const n = game.cells.length;
+
+	game.moves ++;
+	if (row === col) {
+		game.wins[0] += player; // forward diagonal
+	}
+	if ((row + col + 1) === n) {
+		game.wins[1] += player; // backward diagonal
+	}
+	game.wins[col + 2] += player; // horizontal
+	game.wins[row + n + 2] += player; // vertical
+
+	if (game.wins.filter(w => w >= n).length > 0) {
+		document.getElementById('message').textContent = ' "O" won';
+		game.player = 0;
+		return 1;
+	} else if (game.wins.filter(w => (-1 * w) >= n).length > 0) {
+		document.getElementById('message').textContent = ' "X" won';
+		game.player = 0;
+		return -1;
+	} else if (game.moves >= n * n) {
+		document.getElementById('message').textContent = ' Draw';
+		game.player = 0;
+		return 255; // Draw
+	} else {
+		return 0;
+	}
+}
+
+// Event handler for the mouse entering a cell
+function mouseentered(row, col, eid) {
+	if (game.cells[row][col] === 0) {
+		if (game.player > 0) {
+			document.getElementById(eid).textContent = 'O';
+		} else {
+			document.getElementById(eid).textContent = 'X';
+		}
+		document.getElementById(eid).setAttribute("style", "color:#a0a0a0");
+	}
+}
+
+// Event handler for the mouse leaving a cell
+function mouseouted(row, col, eid) {
+	if (game.cells[row][col] === 0) {
+		document.getElementById(eid).textContent = ' ';
+		document.getElementById(eid).setAttribute("style", "color:#424242");
+	}
+}
+
+// Event handler for clicking a cell
+function clicked(row, col, eid) {
+	if (game.player !== 0) { // game.player === 0 if game is not yet started or already finished
+		if (game.cells[row][col] === 0) { // cell value === 0 means unoccupied
+			if (game.player > 0) {
+				document.getElementById(eid).textContent = 'O';
+				game.cells[row][col] = 1;
 			} else {
-				dw ++;
+				document.getElementById(eid).textContent = 'X';
+				game.cells[row][col] = -1;
 			}
-		}
-		if (hv === lgth) {
-			return 1; // Player > 0 won
-		} else if ((-1 * hv) === lgth) {
-			return -1; // Player < 0 won
+			document.getElementById(eid).setAttribute("style", "color:#424242");
+			evaluate(game.player, row, col);
+			game.player = (game.player === -1) ? 1 : -1;
 		}
 	}
-
-	if (df === lgth || db === lgth) {
-		return 1; // Player > 0 won
-	} else if ((-1 * df) === lgth || (-1 * db) === lgth) {
-		return -1; // Player < 0 won
-	}
-
-	// vertical
-	for (let j = 0; j < lgth; j ++) {
-		hv = 0;
-		for (let i = 0; i < lgth; i ++) {
-			if (game.cells[i][j] > 0) {
-				hv ++;
-			} else if (game.cells[i][j] < 0) {
-				hv --;
-			}
-		}
-		if (hv === lgth) {
-			return 1; // Player > 0 won
-		} else if ((-1 * hv) === lgth) {
-			return -1; // Player < 0 won
-		}
-	}
-
-	if (dw === 0) {
-		return 255;
-	}
-	return 0;
 }
 
 function newgame() {
-	let s = document.getElementById('bsize').value;
-	if (s < 3) {
-		s = 3;
+	let n = document.getElementById('bsize').value;
+	// limit the board size to 3x3 to 9x9
+	if (n < 3) {
+		n = 3;
 		document.getElementById('bsize').value = 3;
-	} else if (s > 9) {
-		s = 9;
+	} else if (n > 9) {
+		n = 9;
 		document.getElementById('bsize').value = 9;
 	}
 
-	// 0. prepare game data
-	const cells = new Array(s);
-	const game = {
-		player: -1,
-		cells,
-	};
-	document.getElementById('message').innerHTML = '<div id="message" class="p">&nbsp;</div>';
+	// Update html
+	document.getElementById('message').innerHTML = '<div id="message" class="p">&nbsp;</div>'; // Refrest game message
+	document.getElementById('boardcss').innerHTML = `#board {grid-template-columns: repeat(${n}, 100px);}`; // Build the dynamic part of the CSS
 
-	// 1. build CSS
-	let css = document.getElementById('boardcss');
-	css.innerHTML = `#board {grid-template-columns: repeat(${s}, 100px);}`;
+	// Initialize the Game Status object
+	game.player = -1; // "X" start first
 
-	// 2. build the board
+	game.moves = 0;
+	game.wins = new Array(2 * n + 2);
+	for (let i = 0; i < game.wins.length; i ++) {
+		game.wins[i] = 0;
+	}
+
+	game.cells = new Array(n);
+
+	// Build the game board
 	let board = document.getElementById('board');
 	board.innerHTML = '';
 	let r = -1;
 	let c;
-	for (let i = 0; i < s * s; i ++) {
-		if ((i % s) === 0) {
+	for (let i = 0; i < n * n; i ++) {
+		if ((i % n) === 0) {
 			r ++;
 			c = 0;
-			game.cells[r] = new Array(s);
+			game.cells[r] = new Array(n);
 		}
+
+		// Initialize the cell's corresponding value in the Game Status object
 		game.cells[r][c] = 0;
 		const row = r;
 		const col = c;
 		const eid = `c${row}-${col}`;
 
+		// Create the cell in the html DOM
 		let tmpl = document.createElement('template');
 		tmpl.innerHTML = `<div id="${eid}" class="cell">&nbsp;</div>`;
 		board.appendChild(tmpl.content.firstChild);
 
+		// Add event handlers to the new cell
 		let cell = document.getElementById(eid);
-		cell.onclick = (_) => {
-			if (game.player !== 0) { // Game not yet end
-				if (game.cells[row][col] === 0) {
-					if (game.player > 0) {
-						document.getElementById(eid).textContent = 'O';
-						game.cells[row][col] = 1;
-						game.player = -1;
-					} else {
-						document.getElementById(eid).textContent = 'X';
-						game.cells[row][col] = -1;
-						game.player = 1;
-					}
-					document.getElementById(eid).setAttribute("style", "color:#424242");
-					
-					const rslt = evaluate(game);
-					switch (rslt) {
-						case 0:
-							// Not yet finish
-							break;
-						case 255:
-							// Draw
-							game.player = 0;
-							break;
-						case 1:
-							// Player > 0 won
-							document.getElementById('message').textContent = '"O" won';
-							game.player = 0;
-							break;
-						case -1:
-							// Player < 0 won
-							document.getElementById('message').textContent = '"X" won';
-							game.player = 0;
-							break;
-					}
-				}
-			}
-		};
-		cell.onmouseenter = (_) => {
-			if (game.cells[row][col] === 0) {
-				if (game.player > 0) {
-					document.getElementById(eid).textContent = 'O';
-				} else {
-					document.getElementById(eid).textContent = 'X';
-				}
-				document.getElementById(eid).setAttribute("style", "color:#a0a0a0");
-			}
-		};
-		cell.onmouseout = (_) => {
-			if (game.cells[row][col] === 0) {
-				document.getElementById(eid).textContent = ' ';
-				document.getElementById(eid).setAttribute("style", "color:#424242");
-			}
-		};
+		cell.onclick = (_) => clicked(row, col, eid);
+		cell.onmouseenter = (_) => mouseentered(row, col, eid);
+		cell.onmouseout = (_) => mouseouted(row, col, eid);
 
 		c ++;
 	}
 }
 
 function init() {
-	document.getElementById('bsize').value = 3; // default
-	document.getElementById('new').onclick = (_) => newgame();
-
+	document.getElementById('bsize').value = 3; // default 3x3 game board
+	document.getElementById('bsize').onchange = (_) => newgame(); // Add event handler when the game board size changed
+	document.getElementById('new').onclick = (_) => newgame(); // Add event handler for the 'New game' button
 	newgame();
 }
 
