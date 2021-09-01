07c1c5d046d95669f96adea93b8e1073d77656b2
Paul CT Lai <paul.lai@sea9.org>
2021-09-01 13:12:30 +0800

    Finalize code (#1)
    * Remove the field 'moves' from node
    
    * Improve comments
    
    * Combine row and col into 1 field
    
    * Update comments
    
    * Improve tree tracking
    
    * Add handling of unexplored moves
    
    * Revert UI

--- tictactoe-node.js
+++ tictactoe-node.js
@@ -4,12 +4,10 @@ const EXPLORATION = 1.4142135623730950488016887242097 // √2
 
 // A node in the m.c. tree
 class Node {
-	constructor(p, r, c, m) {
+	constructor(n, p, r, c, m) {
 		this.player = p;
-		this.row = r;
-		this.col = c;
-		this.moves = m;
-		this.next = [];
+		this.pos = (r < 0 || c < 0) ? -1 : n * r + c;
+		this.next = new Array(m);
 		this.runs = 0;
 		this.wins = 0;
 	}
@@ -17,32 +15,56 @@ class Node {
 	// create root node
 	static root(n) {
 		if (n < 3 || n > 9) throw new Error(`Unsupported grid size ${n}`);
-		const root = new Node(0, -1, -1, n * n);
+		const root = new Node(n, 0, -1, -1, n * n);
 		root.grid = n;
 		root.runs = 1;
 		return root;
 	}
 
-	show() {
-		const mapped = [' X', ' _', ' O'];
-		const p = mapped[this.player + 1];
-		const r = this.row < 0 ? ' -' : (''+this.row).padStart(2, ' ');
-		const c = this.col < 0 ? ' -' : (''+this.col).padStart(2, ' ');
-		const m = (''+this.moves).padStart(3, ' ');
-		const n = (''+this.runs).padStart(8, ' ');
-		const w = (''+this.wins).padStart(8, ' ');
-		const l = (''+this.next.length).padStart(3, ' ');
-		return `${p}${r}${c}${m}${l}|${w}/${n}`;
+	row(n) {
+		if (this.pos < 0) return -1;
+		return Math.floor(this.pos / n);
 	}
 
-	// create new child node
-	add(row, col, moves) {
-		const p = (this.player === 0) ? -1 : -1 * this.player; // "X" start first
-		const node = new Node(p, row, col, moves);
-		this.next.push(node);
-		node.parent = this;
-		return node;
+	col(n) {
+		if (this.pos < 0) return -1;
+		return this.pos % n;
 	}
+
+	show(n) {
+		const row = this.row(n);
+		const col = this.col(n);
+
+		const p = [' X', ' _', ' O'][this.player + 1];
+		const r = row < 0 ? ' -' : (''+row).padStart(2, ' ');
+		const c = col < 0 ? ' -' : (''+col).padStart(2, ' ');
+		const u = (''+this.runs).padStart(8, ' ');
+		const w = (''+this.wins).padStart(8, ' ');
+		const m = (''+this.next.length).padStart(3, ' ');
+		const l = (''+this.next.filter(t => t !== undefined).length).padStart(3, ' ');
+		return `${p}${r}${c}|${l}/${m}|${w}/${u}`;
+	}
+
+	// add new child node
+	add(n, row, col, moves) {
+		const p = (this.player === 0) ? -1 : -1 * this.player; // "X" start first
+		const newNode = new Node(n, p, row, col, moves);
+		const index = this.next.findIndex(t => t === undefined);
+		this.next[index] = newNode;
+		newNode.parent = this;
+		return { newNode, index };
+	}
+
+	// // insert new child node
+	// insert(n, index, row, col, moves) {
+	// 	if (index < 0 || index >= this.moves) throw new Error(`Index ${index} out of range ${this.moves}`);
+	// 	if (this.next[index] !== undefined) throw new Error(`Child node ${index} already populated`);
+	// 	const p = (this.player === 0) ? -1 : -1 * this.player; // "X" start first
+	// 	const node = new Node(n, p, row, col, moves);
+	// 	this.next[index] = node;
+	// 	node.parent = this;
+	// 	return node;
+	// }
 
 	// calculate ucb value
 	ucb() {
@@ -51,7 +73,7 @@ class Node {
 		let max = -1.0;
 		const idx0 = [];
 		const idx1 = [];
-		for (let i = 0; i < this.next.length; i ++) {
+		for (let i = 0; i < this.next.filter(t => t !== undefined).length; i ++) {
 			if (this.next[i].runs === 0) {
 				idx0.push(i);
 			} else {
@@ -78,7 +100,7 @@ class Node {
 	}
 }
 
-function show(node, lvl) {
+function show(n, node, lvl) {
 	if (!(node instanceof Node)) throw new Error('Invalid input argument type');
 
 	let buff = '';
@@ -90,14 +112,44 @@ function show(node, lvl) {
 		stck = stack[0];
 		prfx = prfix[0];
 		if (lvl < 0 || (prfx + 1) <= lvl) {
-			stack = stck.next.concat(stack.slice(1));
-			prfix = stck.next.map(_ => prfx + 1).concat(prfix.slice(1));
+			stack = stck.next.filter(t => t !== undefined).concat(stack.slice(1));
+			prfix = stck.next.filter(t => t !== undefined).map(_ => prfx + 1).concat(prfix.slice(1));
 		} else {
 			stack = stack.slice(1);
 			prfix = prfix.slice(1);
 		}
 
-		buff += `${' '.repeat(prfx).padEnd(9, '─')}${prfx} - ${stck.show()}\n`;
+		buff += `${' '.repeat(prfx).padEnd(9, '─')} ${prfx}: ${stck.show(n)}\n`;
 	}
 	return buff;
+}
+
+// walk down the m.c. tree to the node specified by the steps, and return the node if it is sync with the given game
+function sync(root, game) {
+	if (!(root instanceof Node)) throw new Error('Invalid input argument type for Node');
+	if (!(game instanceof Game)) throw new Error('Invalid input argument type for Game');
+	if (root.player !== 0) throw new Error('Root node required');
+	if (!game.steps) throw new Error('The game given is not for interactive use');
+
+	const n = game.grid();
+
+	let leaf = root;
+	for (const step of game.steps) {
+		if (leaf.next.filter(t => t !== undefined).length <= step) {
+			// 'steps' expecting the tree has a node, is an error if turns out not the case
+			throw new Error(`Expecting a child node at position ${step} of node ${leaf.show(n)}`);
+		}
+		// console.log(`Sync... ${step} ${leaf.show(n)}`);
+		leaf = leaf.next[step];
+		if (game.cells[leaf.row(n)][leaf.col(n)] === 0) {
+			console.log(`Not sync 1: ${leaf.show(n)}\n${game.show()}`)
+			return undefined; // game and tree not sync
+		}
+	}
+	if (game.player !== leaf.player) {
+		console.log(`Not sync 2: ${leaf.show(n)}\n${game.show()}`)
+		return undefined; // game and tree not sync
+	}
+
+	return leaf;
 }
--- tictactoe-sim.js
+++ tictactoe-sim.js
@@ -1,4 +1,4 @@
-// Randomly pick an available move base on the state of the current game
+// Pick the first available move base on the state of the current game
 // n - grid size; r - row; c - column:
 // cell index = n*r + c
 function chooseMove(game, node) {
@@ -7,11 +7,12 @@ function chooseMove(game, node) {
 	if (game.player !== -1 && game.player !== 1) throw new Error('Game not in progress');
 
 	const avil = game.availableMoves();
-	const n = avil.length;
+	const m = avil.length;
+	const n = game.grid();
 
 	for (const mov of avil) {
-		if (node.next.filter(v => v.row === mov.r && v.col === mov.c).length <= 0) {
-			mov.m = n - 1;
+		if (node.next.filter(v => v !== undefined && v.row(n) === mov.r && v.col(n) === mov.c).length <= 0) {
+			mov.m = m - 1;
 			return mov;
 		}
 	}
@@ -20,14 +21,20 @@ function chooseMove(game, node) {
 }
 
 // Simulate a game from start to finish
-function sim(root, grid, run) {
+function sim(root, game, run) {
 	if (!(root instanceof Node)) throw new Error('Invalid input argument type');
+	if (!(game instanceof Game)) throw new Error('Invalid input argument type for Game');
 	if (root.player !== 0) throw new Error('Root node required');
 
-	const game = new Game(grid);
+	const n = game.grid();
 	let node = root;
 	let conclusion = 0;
 	let completed = false;
+
+	if (game.steps) { // simulation not starting at the very begining
+		node = sync(root, game);
+		if (!node) throw new Error('Tree not sync with the give game');
+	}
 
 	while (conclusion === 0) {
 		if (!game.nextTurn()) {
@@ -36,16 +43,17 @@ function sim(root, grid, run) {
 		}
 
 		let move;
-		if (node.next.length <= 0 || node.next.length < node.moves) {
+		if (node.next.filter(t => t !== undefined).length < node.next.length) {
 			// leaf node encountered or unvisited move exists, expand the mctree
 			move = chooseMove(game, node);
-			game.makeMove(move.r, move.c); // call this b4 'noode.add()' to ensure the move is valid (position not yet occupied)
-			node = node.add(move.r, move.c, move.m);
+			game.makeMove(move.r, move.c); // call this b4 'add()' to ensure the move is valid (position not yet occupied)
+			const { newNode } = node.add(n, move.r, move.c, move.m);
+			node = newNode;
 			break;
 		} else {
 			// select a move from the tree
 			const idx = node.ucb();
-			move = { r: node.next[idx].row, c: node.next[idx].col };
+			move = { r: node.next[idx].row(n), c: node.next[idx].col(n) };
 			game.makeMove(move.r, move.c);
 			node = node.next[idx];
 		}
@@ -89,68 +97,82 @@ function sim(root, grid, run) {
 	return { run, completed };
 }
 
-const mapped = [' X', ' _', ' O'];
-
-// find in the mctree the current game status
-// move - moves taken in the current game so far
-function track(tree, moves) {
-	let lf = tree;
-	let found = false;
-
-	if (moves.length <= 1 && moves[0].player === lf.player && moves[0].row === lf.row && moves[0].col === lf.col) {
-		found = true;
-		console.log(`[tree] FOUND : ${lf.show()}`);
-	}
-
-  for (let i = 1; i < moves.length; i ++) {
-		console.log(`[move] player: ${mapped[moves[i].player + 1]}${(''+moves[i].row).padStart(2, ' ')}${(''+moves[i].col).padStart(2, ' ')}`);
-
-		found = false
-		for (let j = 0; j < lf.next.length; j ++) {
-			if (moves[i].player === lf.next[j].player && moves[i].row === lf.next[j].row && moves[i].col === lf.next[j].col) {
-				// Found move in mctree
-				found = true;
-				lf = lf.next[j];
-				console.log(`[tree] FOUND : ${lf.show()}`);
-				break;
-			}
-		}
-
-		// Only the latest move in the current game should get a not found, since will add all possible moves in previous runs
-		// Therefore should be error if not found here but not last item in 'moves'
-		if (!found) {
-			if (i != moves.length - 1) throw new Error('Unable to find a move in the m.c. tree before reaching the last move');
-			break;
-		}
-	}
-
-	if (found) {
-		// human player made an existing move, choose a response from the mctree or explore a new one
-		return { leaf: lf, found };
-	} else {
-		// human player made a move not yet explored
-		// TODO: should simulate games till conclusion from the leaf node
-		throw new Error(`Unexplored move ${JSON.stringify(moves[moves.length - 1])} on ${show(lf, 2)}`);
-	}
-}
-
-// Start or continue simulations
-function simulate(grid, runs, tree) {
-	if (!tree) {
+// Start simulations
+function startSim(root, grid, runs) {
+	if (!root) {
 		console.log(`Initializing a new tree of ${grid} x ${grid} games`);
-		tree = Node.root(grid);
+		root = Node.root(grid);
 	}
+	if (root.player !== 0) throw new Error('Root node required');
 
 	let count = 0;
 	for (let i = 0; i < runs; i ++) {
-		const { completed } = sim(tree, grid, i);
+		const { completed } = sim(root, new Game(grid), i);
 		if (completed) count ++;
 	}
 
 	return {
-		tree,
+		tree: root,
 		grid,
 		runs,
 		newly: runs - count,
 	}
+}
+
+// Continue simulation
+function contSim(root, game, runs) {
+	if (!(root instanceof Node)) throw new Error('Invalid input argument type for Node');
+	if (!(game instanceof Game)) throw new Error('Invalid input argument type for Game');
+	if (root.player !== 0) throw new Error('Root node required');
+
+	let count = 0;
+	for (let i = 0; i < runs; i ++) {
+		const { completed } = sim(root, game.clone(), i);
+		if (completed) count ++;
+	}
+
+	return {
+		tree: root,
+		grid: game.grid(),
+		runs,
+		newly: runs - count,
+	}
+}
+
+// find in the mctree the current game status
+// move - moves taken in the current game so far
+function track(root, game, move) {
+	if (!(root instanceof Node)) throw new Error('Invalid input argument type for Node');
+	if (!(game instanceof Game)) throw new Error('Invalid input argument type for Game');
+	if (root.player !== 0) throw new Error('Root node required');
+	if (!game.steps) throw new Error('The game given is not for interactive use');
+
+	const n = game.grid();
+	let found = false;
+	let index; // Index of 'move' in the 'next' array of the current node
+	// let indexNext = -1; // Index of the next move made by the AI player
+
+	// navigate the tree to the current node
+	let leaf = root;
+	for (const step of game.steps) {
+		if (leaf.next.filter(t => t !== undefined).length <= step) {
+			// 'steps' expecting the tree has a node, is an error if turns out not the case
+			throw new Error(`Expecting a child node at position ${step} of node ${leaf.show(n)}`);
+		}
+		leaf = leaf.next[step];
+	}
+
+	// current node found, try to find the latest move in this current node
+	console.log(`[track] move : ${[' X', ' _', ' O'][move.player + 1]}${(''+move.row).padStart(2, ' ')}${(''+move.col).padStart(2, ' ')}`);
+	for (index = 0; index < leaf.next.filter(t => t !== undefined).length; index ++) {
+		if (move.player === leaf.next[index].player && move.row === leaf.next[index].row(n) && move.col === leaf.next[index].col(n)) {
+			found = true; // found latest move in mctree
+			leaf = leaf.next[index];
+			console.log(`[track] found: ${leaf.show(n)}`);
+			break;
+		}
+	}
+
+	if (!found) index = -1; // human player made a move not yet explored
+	return { index, leaf };
 }
